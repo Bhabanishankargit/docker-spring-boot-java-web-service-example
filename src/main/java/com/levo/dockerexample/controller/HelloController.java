bb98c91cb11342b007dcd99266cb159e8ef3ca83
Bhabanishankargit <50334179+Bhabanishankargit@users.noreply.github.com>
2024-09-02 07:13:18 +0530

    Update HelloController.java

--- src/main/java/com/levo/dockerexample/controller/HelloController.java
+++ src/main/java/com/levo/dockerexample/controller/HelloController.java
@@ -9,10 +9,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("docker-java-app")
 public class HelloController {
-	
-	@RequestMapping(value = "/test", method = RequestMethod.GET)
-	public String test() {
-		return "docker-java-app is up and running: " + new Date();
-	}
 
+    // REST Controller Method (Place your customized message here)
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String test() {
+        // Return a customized message
+        return "Hello,Roopa!!-docker-java-app is up and running: " + new Date();
+    }
+
+    // Additional Method (You can add more endpoints for different messages or images)
+    @RequestMapping(value = "/custom-message", method = RequestMethod.GET)
+    public String customMessage() {
+        // Return another customized message
+        return "Hello Bhabani!! at: " + new Date();
+    }
+
+    // Additional Method to Serve Images (Place your image URL here)
+    @RequestMapping(value = "/image", method = RequestMethod.GET, produces = "image/png")
+    public byte[] getImage() {
+        // Load and return an image
+        InputStream in = getClass().getResourceAsStream("/static/images/logo.png");
+        return IOUtils.toByteArray(in);
+    }
 }
